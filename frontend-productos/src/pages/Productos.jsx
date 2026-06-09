c1b707f963e73c903e136d4553ba8a16cae1306a
Brian <bralex176@gmail.com>
2026-06-08 22:26:11 -0600

    fix: min=0 en precio y stock para bloquear negativos

--- frontend-productos/src/pages/Productos.jsx
+++ frontend-productos/src/pages/Productos.jsx
@@ -238,6 +238,8 @@ const Productos = () => {
                     name="precio"
                     value={formulario.precio}
                     onChange={handleChange}
+                    min="0"
+                    step="0.01"
                     required
                   />
 
@@ -261,6 +263,8 @@ const Productos = () => {
                     name="stock"
                     value={formulario.stock}
                     onChange={handleChange}
+                    min="0"
+                    step="1"
                     required
                   />
 
